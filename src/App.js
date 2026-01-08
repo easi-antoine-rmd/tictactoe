965e2909c2f9c8002860bcbd0b1e227f703852f9
easi-antoine-rmd <xeno.antoine@gmail.com>
2026-01-08 17:33:23 +0100

    Creation of a board, added a Square component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Square'
+import { useState } from 'react';
+import Square from './components/Square';
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {board.map((value, index) => {
+        return (
+          <Square
+            key={index}
+            value={value}
+            onSquareClick={() => console.log(index)}
+          />
+        );
+      })}
     </div>
   );
 }
